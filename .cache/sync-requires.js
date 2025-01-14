1f36b1caafd1f59113c5a755fdce2ddf1bd6cf6b
Adit Rastogi <aditrastogi6@gmail.com>
2025-01-14 12:58:23 +0530

    final

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
   "component---src-templates-post-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/src/templates/post.js"))),
   "component---src-templates-tag-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/src/templates/tag.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/src/pages/404.js"))),
   "component---src-pages-archive-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/src/pages/archive.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/adit/Documents/Portfolio-Website/src/pages/index.js"))),
